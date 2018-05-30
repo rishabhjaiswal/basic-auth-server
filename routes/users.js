5f10dc0256e077938dacdb517739682e2a4f3c90
rj1402 <rishabhjaiswal.1402@gmail.com>
2018-05-30 10:13:00 -0700

    added cookies

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json([{id: 1, name: "Jon Doe"},
+  {id: 2, name: "Wong Chigchui"}
+  ]);
 });
 
 module.exports = router;
